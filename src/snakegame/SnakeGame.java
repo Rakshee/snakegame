e1e9311952699e8e8c8f7e4131137a55bdd07aa8
flutter project practice <raksheetripathy6@gmail.com>
2024-10-28 18:28:10 +0530

    1st commit

---
+++ src/snakegame/SnakeGame.java
@@ -0,0 +1,25 @@
+
+package snakegame;
+import javax.swing.*;
+/**
+ *
+ * @author raksheetripathy
+ */
+public class SnakeGame extends JFrame{
+    
+   SnakeGame (){
+       super("Snake Game");
+       add(new Board());
+       pack();
+       //setSize(300,300);
+       setLocationRelativeTo(null);
+       setVisible(true);
+               setResizable(false);
+
+   }
+
+    public static void main(String[] args) {
+        new SnakeGame();
+    }
+    
+}
